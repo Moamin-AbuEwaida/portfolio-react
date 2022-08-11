5c9d205d5cd2a9117b7be4c5238ea389a5994208
Moamin <moamin.salamah@gmail.com>
2022-08-11 09:41:23 +0200

    project is completed but needs customization

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import SocialLinks from "./components/SocialLinks";
 import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
 
 
 function App() {
@@ -9,8 +12,11 @@ function App() {
     <div>
       <Navbar />
       <Home />
-      <SocialLinks />
       <About />
+      <Portfolio />
+      <Experience />
+      <Contact />
+      <SocialLinks />
     </div>
   );
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div
+      name="contact"
+      className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white"
+    >
+      <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Contact Me
+          </p>
+          <p className="py-6">Submit the form below to get in touch with me</p>
+        </div>
+        <div className="flex justify-center items-center">
+          <form
+            action="https://getform.io/f/52accff1-e2be-445d-b8e1-7cf38fb48ccc"
+            method="POST"
+            className="flex flex-col w-full md:w-1/2"
+          >
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter your name"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your email address"
+              className="p-2 my-4 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <textarea
+              name="message"
+              rows="10"
+              placeholder="Enter your message"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            ></textarea>
+            <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
+              Let's talk
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
